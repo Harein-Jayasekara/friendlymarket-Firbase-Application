b3480aea6e9cd3e1a580b56381f33f19c6c4a6ea
Harein-Jayasekara <hareinjaz@gmail.com>
2020-02-29 13:55:15 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# friendlymarket-codelab-wpsd8z
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/friendlymarket-codelab-wpsd8z)
---
+++ src/firebase-config.js
@@ -0,0 +1,28 @@
+/* 
+  Your Firebase config from the Firebase console 
+  https://firebase.google.com/docs/web/setup#config-object
+*/
+const firebaseConfig = {
+  apiKey: 'TODO',
+  authDomain: 'TODO',
+  databaseURL: 'TODO',
+  projectId: 'TODO',
+  storageBucket: 'TODO',
+  messagingSenderId: 'TODO',
+  appId: 'TODO'
+};
+var firebaseConfig = {
+    apiKey: "AIzaSyDnA-24HZWCoy9_ieH7s3QbPMcXPd942sA",
+    authDomain: "friendly-market-3fd87.firebaseapp.com",
+    databaseURL: "https://friendly-market-3fd87.firebaseio.com",
+    projectId: "friendly-market-3fd87",
+    storageBucket: "friendly-market-3fd87.appspot.com",
+    messagingSenderId: "605573879844",
+    appId: "1:605573879844:web:4fe41fd418851cdfc160b1",
+    measurementId: "G-S8PWSKT491"
+  };
+  // Initialize Firebase
+ // firebase.initializeApp(firebaseConfig);
+  //firebase.analytics();
+
+export default firebaseConfig;
